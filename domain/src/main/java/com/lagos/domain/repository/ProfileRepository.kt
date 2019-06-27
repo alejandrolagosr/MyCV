86d5ea1ae39beea7c9f6cad41d1fd224474605b1
Alejandro Lagos <flagos.rd@gmail.com>
2019-06-27 17:00:10 -0500

    New method definitions

--- domain/src/main/java/com/lagos/domain/repository/ProfileRepository.kt
+++ domain/src/main/java/com/lagos/domain/repository/ProfileRepository.kt
@@ -1,10 +1,17 @@
 package com.lagos.domain.repository
 
+import com.lagos.domain.models.Curriculum
+import com.lagos.domain.models.Education
+import com.lagos.domain.models.Experience
 import com.lagos.domain.models.Profile
 import io.reactivex.Single
 
 interface ProfileRepository {
 
+    fun getCurriculum(): Single<Curriculum>
     fun getProfile(): Single<Profile>
+    fun getEducation(): Single<List<Education>>
+    fun getExperience(): Single<List<Experience>>
+    fun getSkills(): Single<String>
 
 }
