82279c4ce6dc8bc2afb86bf8af4f199126978a12
Alejandro Lagos <flagos.rd@gmail.com>
2019-06-27 19:44:38 -0500

    Updated gist url

--- data/src/main/java/com/lagos/data/network/ApiGistInterface.kt
+++ data/src/main/java/com/lagos/data/network/ApiGistInterface.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 
 interface ApiGistInterface {
 
-    @GET("alejandrolagosr/cc2871f6c0dd8f84d15fc564078e608b/raw/a183e0db539e7421fb2238209d8137fc9f48521b/profile.json")
+    @GET("alejandrolagosr/cc2871f6c0dd8f84d15fc564078e608b/raw/db05c2c2738380a1d822f2fac74f2209752ff1ee/profile.json")
     fun getProfileData(): Observable<ProfileModel>
 }
